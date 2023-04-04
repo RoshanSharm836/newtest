6c53d5719356c4831ee9559efb49fcceb537f246
Roshanrocked <roshna837kumar@gmail.com>
2023-04-05 01:08:54 +0530

    done

--- Routes/test.model.js
+++ Routes/test.model.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
   console.log("data");
   try {
     const data = await test.find();
-    return res.status(200).send(data);
+    return res.status(200).send("data");
   } catch (error) {
     res.status(500).send(error.message);
   }
