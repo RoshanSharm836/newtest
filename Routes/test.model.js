863fb048366dd4945e2a5a07aceba4074d6c4ec9
Roshanrocked <roshna837kumar@gmail.com>
2023-04-05 01:11:14 +0530

    done

--- Routes/test.model.js
+++ Routes/test.model.js
@@ -4,10 +4,19 @@ const test = require("../Schema/test.schema");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log("data");
   try {
     const data = await test.find();
-    return res.status(200).send("data");
+    console.log("data", data);
+    return res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const data = await test.create(req.body);
+    return res.status(200).send({ data: "posted" });
   } catch (error) {
     res.status(500).send(error.message);
   }
